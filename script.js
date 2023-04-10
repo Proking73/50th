4eb0ca29cc16ee7f217286dfc7a577904fe207c8
77035338 <MY_NAME@example.com>
2023-04-09 23:14:40 -0400

    Next -> Life story -> Balloons (Sticky)

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 h = document.querySelector(".happy");
 d = document.querySelector(".gold");
 b = document.querySelector(".birthday");
+a = document.querySelector("header::after");
+
+document.addEventListener('scroll', () => {
+    //if variable animation computed style
+    var reveals = document.querySelectorAll(".reveal")
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 150;
+        if (elementTop < windowHeight - elementVisible) {
+          reveals[i].classList.add("active");
+        } else {
+          reveals[i].classList.remove("active");
+        }
+      }
+});
