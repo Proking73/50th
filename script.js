1abdb78d72fb9e39fed30543b459d4462fac9e99
77035338 <MY_NAME@example.com>
2023-04-12 23:04:15 -0400

    Thursday: Life story, fix arrow Friday: Scroll bar with taps id, balloons, sound effects

--- script.js
+++ script.js
@@ -2,19 +2,18 @@ h_t = document.querySelector(".happy");
 d_a = document.querySelector(".gold");
 b_t = document.querySelector(".birthday");
 a = window.getComputedStyle(document.querySelector(":root")).getPropertyValue('--arrow');
-console.log(a)
 
-document.body.addEventListener('scroll', () => {
+window.addEventListener('scroll', () => {
     //if variable animation computed style
     var reveals = document.querySelectorAll(".reveal");
     document.querySelector("header").addEventListener('animationend', () => {
-      document.querySelector(":root").style.setProperty('--a-state', ' paused');
+      document.querySelector(":root").style.setProperty('--arrow', '  barrow 4s ease-in-out 0s 1 forwards');
     });
 
     for (var i = 0; i < reveals.length; i++) {
         var windowHeight = window.innerHeight;
         var elementTop = reveals[i].getBoundingClientRect().top;
-        var elementVisible = 200;
+        var elementVisible = 300;
         if (elementTop < windowHeight - elementVisible) {
           reveals[i].classList.add("active");
         } else {
