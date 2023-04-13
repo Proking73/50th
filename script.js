800d855cfe60dd1ce0cee23e1e9f8026e97a6b36
77035338 <MY_NAME@example.com>
2023-04-12 20:59:52 -0400

    Balloons -> Images -> Text, music

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
-h = document.querySelector(".happy");
-d = document.querySelector(".gold");
-b = document.querySelector(".birthday");
-a = document.querySelector("header::after");
+h_t = document.querySelector(".happy");
+d_a = document.querySelector(".gold");
+b_t = document.querySelector(".birthday");
+a = window.getComputedStyle(document.querySelector(":root")).getPropertyValue('--arrow');
+console.log(a)
 
-document.addEventListener('scroll', () => {
+document.body.addEventListener('scroll', () => {
     //if variable animation computed style
-    var reveals = document.querySelectorAll(".reveal")
+    var reveals = document.querySelectorAll(".reveal");
+    document.querySelector("header").addEventListener('animationend', () => {
+      document.querySelector(":root").style.setProperty('--a-state', ' paused');
+    });
+
     for (var i = 0; i < reveals.length; i++) {
         var windowHeight = window.innerHeight;
         var elementTop = reveals[i].getBoundingClientRect().top;
-        var elementVisible = 150;
+        var elementVisible = 200;
         if (elementTop < windowHeight - elementVisible) {
           reveals[i].classList.add("active");
         } else {
-          reveals[i].classList.remove("active");
+          //reveals[i].classList.remove("active");
         }
       }
 });
